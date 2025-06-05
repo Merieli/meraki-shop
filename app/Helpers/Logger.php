79e7895583065de4f34cdec1edffc0d722fbb43a
Merieli Manzano <merieli.manzano@tray.net.br>
2025-06-05 07:18:51 -0300

    feat: add info in Logger

--- app/Helpers/Logger.php
+++ app/Helpers/Logger.php
@@ -8,6 +8,11 @@ class Logger
 {
     private $divider = PHP_EOL . "==============================================" . PHP_EOL;
 
+    public function info(string $msg, array $context = [], string $code = "UNKNOWN")
+    {
+        Log::info("$code | $msg" . PHP_EOL, [...$context, $this->divider]);
+    }
+    
     public function critical(string $msg, array $context = [], string $code = "UNKNOWN")
     {
         Log::critical("$code | $msg" . PHP_EOL, [...$context, $this->divider]);
@@ -17,4 +22,10 @@ class Logger
     {
         Log::error("$code | $msg" . PHP_EOL, [...$context, $this->divider]);
     }
+
+    public function debug(string $msg, array $context = [], string $code = "UNKNOWN")
+    {
+        Log::debug("$code | $msg" . PHP_EOL, [...$context, $this->divider]);
+    }
+    
 }
