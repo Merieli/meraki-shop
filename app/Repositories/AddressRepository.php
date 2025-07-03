d1d5023106ffc687e911fe6c83828d9c57a7d899
Merieli Manzano <merieli.amanzano@gmail.com>
2025-07-02 21:05:07 -0300

    feate: add OrderRepository and AddressRepository

---
+++ app/Repositories/AddressRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MerakiShop\Repositories;
+
+use MerakiShop\Contracts\Repositories\AddressRepositoryInterface;
+use MerakiShop\Http\Controllers\Controller;
+
+class AddressRepository extends Controller implements AddressRepositoryInterface
+{
+
+}
